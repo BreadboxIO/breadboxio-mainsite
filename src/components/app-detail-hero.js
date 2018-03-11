c5847d2b21c21484f616a0622c6dea6ff2836065
Joey Schroeder <schrjos@gmail.com>
2018-03-11 14:28:47 -0500

    updated ROD page with new images;

--- src/components/app-detail-hero.js
+++ src/components/app-detail-hero.js
@@ -18,7 +18,7 @@ export class AppDetailHero extends Component {
                     />
                     <div className='app-detail-hero__content__leader'>
                         <h1>Your safety.<br />Made easy.</h1>
-                        <p>Instantly know <em>wheather</em> the <em>weather</em> is alright for riding.</p>
+                        <p>Instantly know <em>whether</em> the <em>weather</em> is alright for riding.</p>
                     </div>
                     <div className='app-detail-hero__content__store-links'>
                         {appStoreLink && <a href={appStoreLink} target='_blank'><Icon name='apple' /> App Store</a>}
--- src/screens/ride-or-die-app.js
+++ src/screens/ride-or-die-app.js
@@ -26,7 +26,7 @@ export class RideOrDieAppScreen extends Component {
                         <h2>The Everyday Ride Safety App</h2>
                         <p>Never get caught in dangerous weather while motorcycle or bike riding again!</p>
                         <p>Just one tap away! Open up <strong>{appName}</strong> and instantly know <em>whether</em> the current <em>weather</em> conditions are comfortable for a motorcycle or bike ride.</p>
-                        <p>Tap the "Current Mode" button to switch into "Commute Mode." In Commute Mode, <strong>{appName}</strong> will utilize your commute times and your home/work locations to estimate "whether" the "weather" will match your riding preferences when you're leaving for and returning from work.</p>
+                        <p>Swipe left and right to switch between your current ride conditions and your commute ride conditions. In Commute Mode, <strong>{appName}</strong> will utilize your commute times and your home/work locations to estimate <em>whether</em> the <em>weather</em> will match your riding preferences when you're leaving for and returning from work.</p>
                         <p>Flip the "Language" setting to "Explicit" for a more aggressive ride message.</p>
                         <div>
                             <div className='app-detail-features__feature'>
