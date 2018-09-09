2f749fb7178859ec5ca0f3b6f38e736dfbe1ea9b
Joey Schroeder <schrjos@gmail.com>
2018-09-09 11:26:13 -0500

    feat: Update default invoice item rate

--- src/classes/invoice-item.js
+++ src/classes/invoice-item.js
@@ -2,7 +2,7 @@ export class InvoiceItem {
     constructor(data = {}) {
         this.hours = data.hours || 0;
         this.description = data.description || 'Product maintenance';
-        this.rate = data.rate || 100;
+        this.rate = data.rate || 150;
     }
 
     getTotalCost() {
